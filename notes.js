c0c862b121a40f6756ef2de07690fd9039029dd8
Adebayo Kehinde <press.spark@gmail.com>
2018-08-15 16:12:50 +0100

    Reading notes and reusability

--- notes.js
+++ notes.js
@@ -37,7 +37,9 @@ var getAll = () => {
 }
 
 var readNote = (title) => {
-    console.log('Reading note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title)
+    return filteredNotes[0]
 }
 
 var removeNote = (title) => {
@@ -52,9 +54,16 @@ var removeNote = (title) => {
     return notes.length !== filteredNotes.length;
 }
 
+var logNote = (note) => {
+    console.log('---')
+    console.log(`Title: ${note.title} `)
+    console.log(`Body: ${note.body} `)
+}
+
 module.exports = {
         addNote,
         getAll,
         readNote,
-        removeNote
+        removeNote,
+        logNote
 }
